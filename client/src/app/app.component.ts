46b5ef4e933ab8d58558a3c0ce75a1ae26dde072
Jordi Riera <jordiriera81@gmail.com>
2022-05-08 11:16:43 +0200

    fix:Improve path naming

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,7 +8,7 @@ import {Observable} from "rxjs";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit,OnDestroy{
-  title = 'angular-client';
+  title = 'client';
   receivedMessage$!:Observable<string>
 
 
